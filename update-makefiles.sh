677e6b4200eab33af0d94b17ab931dd5ac0931cd
Gianmarco <gianmarcoscarano@gmail.com>
2020-04-10 19:55:46 +0000

    interfaces: remove conflict libs *They conflict with LOS, which now we use instead of Arrow
    Signed-off-by: Gianmarco <gianmarcoscarano@gmail.com>

--- update-makefiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
-
-do_makefiles_update \
-  "vendor.arrow:hardware/arrow/interfaces" \
-  "android.hardware:hardware/interfaces" \
-  "android.hidl:system/libhidl/transport"
